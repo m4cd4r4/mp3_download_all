8f181f69a51c69c061fea05c732ecf283e3420d7
m4cd4r4 <47749761+m4cd4r4@users.noreply.github.com>
2022-07-10 22:05:42 +0800

    Replace [-----ENTER URL HERE-----]
    Enter the URL that contains your list of MP3s in place of the text in square brackets on line 5

---
+++ mp3_download_all.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+r = requests.get([---------ENTER WEBSITE HERE-----------])
+soup = BeautifulSoup(r.text, 'html.parser')
+
+with requests.Session() as req:
+    for item in soup.select("#playlist"):
+        for href in item.findAll("a"):
+            href = href.get("href")
+            name = re.search(r"([^\/]+$)", href).group()
+            if '.' not in name[-4]:
+                name = name[:-3] + '.mp3'
+            else:
+                pass
+            print(f"Downloading File {name}")
+            download = req.get(href)
+            if download.status_code == 200:
+                with open(name, 'wb') as f:
+                    f.write(download.content)
+            else:
+                print(f"Download Failed For File {name}")
